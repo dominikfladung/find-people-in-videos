80319af50d5cc6dedfe31f9ae000dfd520338048
Dominik Fladung <dominik.fladung@gmx.de>
2022-11-29 23:55:30 +0100

    WIP try to train face model

--- src/examples/HOGWebcam.py
+++ src/examples/HOGWebcam.py
@@ -15,7 +15,7 @@ cap = cv2.VideoCapture(0)
 
 # the output will be written to output.avi
 out = cv2.VideoWriter(
-    'output.avi',
+    '../../output/output.avi',
     cv2.VideoWriter_fourcc(*'MJPG'),
     15.,
     (640, 480))
