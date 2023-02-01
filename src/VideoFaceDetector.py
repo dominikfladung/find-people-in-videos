98229d21cd3609936eac371846b229b60df14656
Dominik Fladung <dominik.fladung@gmx.de>
2023-02-02 00:12:10 +0100

    Export Video

--- src/VideoFaceDetector.py
+++ src/VideoFaceDetector.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+import time
 import cv2
 import imageio
 
@@ -11,22 +12,31 @@ class VideoFaceDetector(FaceDetector):
 
         # initialize the video capture
         filename = 'videos/got.mp4'
+        output_filename = 'videos/detected.got.mp4'
         video = imageio.get_reader(filename,  'ffmpeg')
+        meta = video.get_meta_data()
+        video_writer = imageio.get_writer(output_filename, fps=meta['fps'])
+        max_frame_index = round(meta['duration'] * meta['fps'])
 
-        for frame in video:
+        for i, frame in enumerate(video):
             face_detections = self.recognize(frame)
             print(face_detections)
 
-            # show the frame
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+            self.display_frame(frame, i, max_frame_index)
+            video_writer.append_data(frame)
 
+            key = cv2.waitKey(1) & 0xFF
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
                 break
 
         # do a bit of cleanup
         cv2.destroyAllWindows()
+        video_writer.close()
+
+    def display_frame(self, frame, i, max_frame_index):
+        cv2.imshow("Frame", frame)
+        cv2.setWindowTitle('Frame', str(i) + ' / ' + str(max_frame_index))
 
 
 if __name__ == "__main__":
